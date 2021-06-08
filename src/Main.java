686580fa8fbc584039f40c2fafbe61bcca647575
Nikoloz Imerlishvili <65135792+callmenikk@users.noreply.github.com>
2021-06-08 22:51:15 +0400

    Comments
    removed comments

--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,11 @@ public class Main {
     Scanner scanner = new Scanner(System.in);
     
     System.out.println("Enter side x: "); 
-    x = scanner.nextDouble(); 
+    x = scanner.nextDouble();
     System.out.println("Enter side y: "); 
     y = scanner.nextDouble(); 
         
-    z = Math.sqrt((x*x)+(y*y));
+    z = Math.sqrt((x*x)+(y*y));   
    
     System.out.println("The hypotenuse is :"+z); 
     
